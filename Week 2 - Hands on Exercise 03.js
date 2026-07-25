3a27b0f373693b6ddb6a6af7900f439c2e1703c8
sal-2529 <sxmarin17@gmail.com>
2026-07-25 06:44:54 -0700

    Week 2 - Hands on Exercise

---
+++ Week 2 - Hands on Exercise 03.js
@@ -0,0 +1,18 @@
+// Week 2 - Hands on Exercise 03
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log(i + " Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log(i + " Buzz");
+    }
+    else {
+        console.log(i);
+    }
+
+}
